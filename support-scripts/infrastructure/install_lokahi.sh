0ca14bf79360b0d5672368822747b010e633c78d
Morteza E <105240903+mershad-manesh@users.noreply.github.com>
2024-04-11 10:33:28 -0400

    updated file

--- support-scripts/infrastructure/install_lokahi.sh
+++ support-scripts/infrastructure/install_lokahi.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+msg(){
+    echo "$1" | tee -a  "${LOGFILE}" 2>&1
+}
+
 SECONDS=0
 EXECTIME="$(date +%Y%m%d_%H%M%S)"
 
@@ -18,12 +22,19 @@ INGRESSVALUEFILE="../../examples/ingress-nginx-values.yaml"
 
 
 sed "s/  hostname: onmshs/  hostname: $DOMAIN/g" ${LOKAHIVALUEFILE} > ../tmp/lokahi.yaml
+if [ "$NAMESPACE" != "default" ]; then
+msg "Making namespace modifications"
+sed "s/      nginx.ingress.kubernetes.io\/auth-tls-secret: default\/client-root-ca-certificate/      nginx.ingress.kubernetes.io\/auth-tls-secret: ${NAMESPACE}\/client-root-ca-certificate/g" ../tmp/lokahi.yaml > ../tmp/lokahi2.yaml
+sed "s/      nginx.ingress.kubernetes.io\/auth-url: \"http:\/\/opennms-minion-certificate-verifier.default.svc.cluster.local:8080\/certificate\/debug\"/      nginx.ingress.kubernetes.io\/auth-url: \"http:\/\/opennms-minion-certificate-verifier.${NAMESPACE}.svc.cluster.local:8080\/certificate\/debug\"/g" ../tmp/lokahi2.yaml > ../tmp/lokahi.yaml
+rm ../tmp/lokahi2.yaml
+fi
 LOKAHIVALUEFILE="../tmp/lokahi.yaml"
 
+if [ ! -s "$LOKAHIVALUEFILE" ]; then
+msg "The file '${LOKAHIVALUEFILE}' is empty."
+exit 1
+fi
 
-msg(){
-    echo "$1" | tee -a  "${LOGFILE}" 2>&1
-}
 
 msg "Lokahi Installation Script" 
 msg "EXECTIME: ${EXECTIME}"
